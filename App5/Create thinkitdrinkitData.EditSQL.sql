757d365bb1dd652bafeee246a7b9f90b4ab76ac3
milolang <milolang@gmail.com>
2015-04-24 13:20:13 -0400

    Revert "Revert "db not showing decimal places""
    This reverts commit 2ac3f7f97ac97548122f9014a4cec773bd2adbb0.

--- App5/Create thinkitdrinkitData.EditSQL.sql
+++ App5/Create thinkitdrinkitData.EditSQL.sql
@@ -3,11 +3,12 @@ GO
 
 UPDATE thinkitdrinkitData.Boost
 Set 
-	--[Name] = 'VinOserae', 
-	[VendID] = '',
-	[Image] = '',
-	[Label] = '',
-	[Price] = '',
-	[InfoLite] = ''
+	--[Name] = 'Fat Burner' 
+	[VendID] = 'b8ca3a65-0166-11e4-fbb5-a71027a53b09',
+	--[Image] = '',
+	--[Label] = '',
+	[Price] = '0.47',
+	[Info] = 'http://storeapp.thinkitdrinkit.com/teacrine/',
+	[InfoLite] = 'TeaCrine works with the body’s natural metabolic processes to deliver energy, increase mental clarity, and improve motivation, and mood.'
 
-WHERE Name = 'Vinoserae'
+WHERE Name = 'TeaCrine'
