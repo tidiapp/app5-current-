7bdad35041d55f3d5343126259fe829300290ac2
milolang <milolang@gmail.com>
2015-05-12 12:10:37 -0400

    test5 node module added

---
+++ App5/App5/mobile_services/connect.js
@@ -0,0 +1,13 @@
+﻿// http://msdn.microsoft.com/en-us/library/azure/jj554207.aspx
+var thinkitdrinkitDataClient = new WindowsAzure.MobileServiceClient(
+                "https://thinkitdrinkitdata.azure-mobile.net/",
+                "lJAQKEvRAcYGsFuiggkyaxkkwqcywN52");
+
+// Add a filter that adds a header to prevent caching. This makes sure that the 
+// latest data is returned when the 'Refresh; button is clicked.        
+var noCachingFilter = function (request, next, callback) {
+    if (request.type === 'GET' && !request.headers['If-Modified-Since']) {
+        request.headers['If-Modified-Since'] = 'Mon, 27 Mar 1972 00:00:00 GMT';
+    }
+    next(request, callback);
+};
