6fc82b9c46ebcc93b9e33947d8f018a65986860e
milolang <milolang@gmail.com>
2015-06-05 16:08:32 -0400

    surface version scalled base page & default page

--- App5/Create thinkitdrinkitData.EditSQL.sql
+++ App5/Create thinkitdrinkitData.EditSQL.sql
@@ -2,19 +2,19 @@
 USE [thinkitdrinkitData_db]
 GO
 
-UPDATE thinkitdrinkitData.Base
+UPDATE thinkitdrinkitData.Boost
 Set 
     --Name] = 'Oryzatein Ultra80', 
-	--[VendID] = 'b8ca3a65-0166-11e4-fbb5-a71d663b504e',
-	--[Image] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitappimagesbase/Protein_Recovery.png',
-	[Price] = '21.00'
-	--[Label] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitlabelsbase/MensBeautyBase.jpg',
+	[VendID] = 'dc85058a-a666-11e4-ef46-e8fea10d4aca',
+	--[Image] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitappimagesboost/Bacognize_SM.png',
+	[Price] = '11.85',
+	[Label] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitlabelsboost/Fat-Burner.jpg',
 
-	--[Info] = 'http://storeapp.thinkitdrinkit.com/phwhey/',
-	--[InfoLite] = 'Partially hydrolyzed instantized whey protein isolate is high in leucine, a bioactive peptides. It is an all-natural, patented ingredient for healthy weight loss and weight management. It has been clinically proven to increase fat loss, retain lean mass and and lower glucose response. It is a complete, high quality protein with a rich amino acid (AA) profile including branched chain amino acids (BCAA). Key features: high in protein low in fat; inhibits fat deposition; increases satiety; improves overall body composition.'
+	[Info] = 'http://storeapp.thinkitdrinkit.com/fatburner/',
+	[InfoLite] = 'The combination of Sinetrol and Cravingz’Gone acts as a fat burner, and helps to decrease abdominal body fat, waist size and hip size. Key features:increases burning of fat; helps to decrease waist size; increases weight loss.'
 	--[BaseDBbase_id] = '138'
 	--[FuncDBfunc_id] = '6'
-    --Order] = '42'
-WHERE Name = 'Hemp' 
+    --[Order] = '5'
+WHERE Name = 'Fat Burner' 
 	  --AND id 'blah'
 	  --id BETWEEN '14462' and '14608'
