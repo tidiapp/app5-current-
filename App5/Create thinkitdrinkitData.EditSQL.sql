cf5cb45d16ba11c6a3c31af16b95520bd127b78d
linnernrivas <linnernrivas@aol.com>
2015-07-30 10:20:27 -0400

    Data 1

--- App5/Create thinkitdrinkitData.EditSQL.sql
+++ App5/Create thinkitdrinkitData.EditSQL.sql
@@ -4,19 +4,19 @@ GO
 
 UPDATE thinkitdrinkitData.Boost
 Set 
-    --[Name] = 'AminoBlast® Instanized BCAA Powder', 
+    [Name] = ' ACTIValoe® ', 
 	--[VendID] = 'b8ca3a65-0166-11e4-fbb5-a6fc14a2f85c'
 	--[Image] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitappimagesboost/GivoCal_Calcium_SM.png'
 	--[Price] = '2.90',
 	--[Label] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitlabelsboost/Givocal.jpg'
 
 	--[Info] = 'http://storeapp.thinkitdrinkit.com/givocal/',
-	--[InfoLite] = 'Calcium is critical for healthy bone structure and density as well as cellular functions and metabolisms. GivoCal is a “2-in-1” ingredient that brings calcium and phosphorus in a 1.3 ratio necessary for good calcium absorption to support bone tissue and cellular functions, and has a suggested higher bioavailability than calcium carbonate.'
+	[InfoLite] = 'ACTIValoe®  contains bioactive component of aloe vera. It supports overall health for athletes by boosting the immune system, preserving digestive health and speeding up cellular recovery. Key features: supports immune health by eliminating bacteria in the body; speeds up healing of wounds and injuries; sustain workouts with immune and digestive defense.'
 	--[BaseDBbase_id] = '138'
 	--[FuncDBfunc_id] = '6'
-    [Order] = '46'
+    --[Order] = '46'
 	--[ShopifyID] = '123',
 	--[ShopifyImg] = 'httpbbbbbb'
-WHERE Name = 'Arla Whey Isolate 9213*' 
+WHERE Name = 'ACTIValoe' 
 	  --AND id 'blah'
 	  --id BETWEEN '14462' and '14608'
