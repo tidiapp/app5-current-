fcc5c4933d6601aaada94a084086e80a05414462
linnernrivas <linnernrivas@aol.com>
2015-09-29 17:58:05 -0400

    price

--- App5/Create thinkitdrinkitData.EditSQL.sql
+++ App5/Create thinkitdrinkitData.EditSQL.sql
@@ -2,14 +2,14 @@
 USE [thinkitdrinkitData_db]
 GO
 
-UPDATE thinkitdrinkitData.Boost
+UPDATE thinkitdrinkitData.Flavor
 Set 
 	--[ecommerceinfolite]='Organic Oryzatein™ is a whole grain brown rice protein concentrate. It is the first and only allergen-friendly plant protein. It is the smoothest form of whole grain rice proteins, available at 80% protein concentration with an excellent amino acid profile.'
 --’
 	--[Name] = 'GIVOCAL™*',
 	--[VendID] = 'b8ca3a65-0166-11e4-fbb5-a7024fa4f6a5',
 	--[Image] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitappimagesboost/Quercetin_CellularEnergy_SM.png'
-	[Price] = '5.25'
+	[Price] = '5'
 	--[Label] = 'http://thinkitdrinkit.blob.core.windows.net/thinkitdrinkitlabelsboost/Givocal.jpg'
 
 	--[Info] = 'http://storeapp.thinkitdrinkit.com/givocal/',
@@ -20,7 +20,7 @@ Set
 	--[ShopifyID] = '123',
 	--[ShopifyImg] = 'httpbbbbbb'
 WHERE  
-	 Name = 'Wellmune®*'	
+	 Name = 'Pom Acai caloric'	
 	  --id='66'
 	  --AND id 'blah'
 	  --id BETWEEN '14462' and '14608'
